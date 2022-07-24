a9d931f5ea8aabc242e238d6365fdfbf9f6e7393
Nicolas Morin <nicolas.morin@reacteev.com>
2022-07-25 00:35:29 +0200

    Allow overriding excluded paths (to include offline-plugin-app-shell-fallback)

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -106,9 +106,7 @@ export const onRenderBody = (
   let excludePaths = ['/offline-plugin-app-shell-fallback/']
 
   if (pluginOptions && typeof pluginOptions.exclude !== 'undefined') {
-    pluginOptions.exclude.map((exclude) => {
-      excludePaths.push(exclude)
-    })
+    excludePaths = pluginOptions.exclude;
   }
 
   const isPathExcluded = excludePaths.some((path) => pathname === path)
